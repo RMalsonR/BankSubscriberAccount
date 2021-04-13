9b819de343fa67eda0ea9b7d0be0b5f3e4e9271a
Renat Thaher <rinatthaher9@gmail.com>
2021-04-13 14:22:53 +0300

    small fix

--- core/tasks.py
+++ core/tasks.py
@@ -15,7 +15,7 @@ class SubtractHoldTask(PeriodicTask):
     def run(self, *args, **kwargs):
         logger.debug('Start Celery task: SubtractHoldTask')
         try:
-            service = SubtractHoldFlow(100)
+            service = SubtractHoldFlow()
         except BaseException as e:
             logger.error(f'Get unexpected error during celery task: {e}')
         logger.debug('Finish Celery task: SubtractHoldTask')
